bab21fe504161e46bbfa81594cfb1201c4b43c7f
ttanachot <StupidCupid@StupidCupid.local>
2013-10-05 20:49:03 +0700

    Change new Login UI

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,33 +1,122 @@
-<h1><?php echo lang('login_heading');?></h1>
-<p><?php echo lang('login_subheading');?></p>
+<html lang="en"><head>
+    <meta charset="utf-8">
+    <title>TREX Corp.</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-<div id="infoMessage"><?php echo $message;?></div>
+    <!-- Le styles -->
+    <link href="<?php echo base_url(); ?>assets/css/bootstrap.css" rel="stylesheet">
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+        padding-bottom: 40px;
+      }
+      .sidebar-nav {
+        padding: 9px 0;
+      }
 
-<?php echo form_open("auth/login");?>
+      @media (max-width: 980px) {
+        /* Enable use of floated navbar text */
+        .navbar-text.pull-right {
+          float: none;
+          padding-left: 5px;
+          padding-right: 5px;
+        }
+      }
+    </style>
+    <link href="<?php echo base_url(); ?>assets/css/bootstrap-responsive.css" rel="stylesheet">
 
-  <p>
-    <?php echo lang('login_identity_label', 'identity');?>
-    <?php echo form_input($identity);?>
-  </p>
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="../assets/js/html5shiv.js"></script>
+    <![endif]-->
 
-  <p>
-    <?php echo lang('login_password_label', 'password');?>
-    <?php echo form_input($password);?>
-  </p>
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
+                    <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
+                                   <link rel="shortcut icon" href="assets/ico/favicon.png">
+  </head>
 
-  <p>
-    <?php echo lang('login_remember_label', 'remember');?>
-    <?php echo form_checkbox('remember', '1', FALSE, 'id="remember"');?>
-  </p>
+  <body>
 
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container-fluid">
+          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="brand" href="#">TREX Corp.</a>
+          <div class="nav-collapse collapse">
+            <ul class="nav">
+              <li class="active"><a href="#">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#contact">Contact</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>
 
-  <p><?php echo form_submit('submit', lang('login_submit_btn'));?></p>
+    <div class="container-fluid">
+      <div class="row-fluid">
+        <div class="span3 offset4">
+        <div class="well">
+          <!--<h1><?php echo lang('login_heading');?></h1>-->
+            <!--<p><?php echo lang('login_subheading');?></p>-->
 
-<?php echo form_close();?>
+            <?php echo form_open("auth/login");?>
+            
+            <h1>TREX Corp.</h1><br>
+            <input type="text" class="input-large" placeholder="Email/Username" name="identity">
+            <br> 
+            <input type="password" class="input-large" placeholder="Password" name="password">
+          
+            <p>Remember Me: <?php echo form_checkbox('remember', '1', FALSE, 'id="remember"');?></p>
 
-<p><a href="forgot_password"><?php echo lang('login_forgot_password');?></a></p>
+            <button class="btn btn-primary" type="submit">Login</button>
 
+            <?php echo form_open('auth/create_user'); ?>
+              <button class="btn" type="submit">Register</button>
+            <?php echo form_close(); ?>
 
-<?php echo form_open('auth/create_user'); ?>
-  <input type="submit" value="Register">
-<?php echo form_close(); ?>
+            <p><a href="forgot_password"><?php echo lang('login_forgot_password');?></a></p>
+            
+            <div id="infoMessage" style="color:red"><?php echo $message;?></div>
+            
+          </div><!---span-->
+          </div><!--row-->
+        </div> 
+       
+      <hr>
+
+      <footer>
+        <p>© TREX Corp. 2013</p>
+      </footer>
+
+    </div><!--/.fluid-container-->
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/bootstrap-transition.js"></script>
+    <script src="assets/js/bootstrap-alert.js"></script>
+    <script src="assets/js/bootstrap-modal.js"></script>
+    <script src="assets/js/bootstrap-dropdown.js"></script>
+    <script src="assets/js/bootstrap-scrollspy.js"></script>
+    <script src="assets/js/bootstrap-tab.js"></script>
+    <script src="assets/js/bootstrap-tooltip.js"></script>
+    <script src="assets/js/bootstrap-popover.js"></script>
+    <script src="assets/js/bootstrap-button.js"></script>
+    <script src="assets/js/bootstrap-collapse.js"></script>
+    <script src="assets/js/bootstrap-carousel.js"></script>
+    <script src="assets/js/bootstrap-typeahead.js"></script>
+
+  
+
+</body></html>
