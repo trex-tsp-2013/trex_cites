918b2dd006cba2683a06d8213fa5a494e1e7f8a9
Nuttapon <nut@Nuttapons-MacBook-Pro.local>
2013-10-06 15:02:44 +0700

    Register UI
    Adding up UI for register and some button

--- application/views/pages/login.php
+++
@@ -1,5 +0,0 @@
-<?php echo form_open('pages/login'); ?>
-	Username: <input type="text" size="20" id="username" name="username"><br>
-  	Password: <input type="password" size="20" id="password" name="password"><br>
-  <input type="submit" value="Login">
-<?php echo form_close(); ?>
