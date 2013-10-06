3593fd79995ab6b21f23cdd1a16945b68f7283a2
ttanachot <StupidCupid@StupidCupid.local>
2013-10-07 00:53:11 +0700

    Update Controller
    update save15 method

--- application/controllers/pages_c.php
+++ application/controllers/pages_c.php
@@ -50,6 +50,58 @@ class Pages_C extends CI_Controller{
 		}
 		
 	}
+	public function save15()
+	{
+
+		if ($this->ion_auth->logged_in())
+		{
+			$data15 = array(
+				'fname' => $this->input->post('fname'),
+				'fbd' => $this->input->post('fbd'),
+				'fnation' => $this->input->post('fnation'),
+				'fpassport' => $this->input->post('fpassport'),
+				'fissue' => $this->input->post('fissue'),
+				'faddress' => $this->input->post('faddress'),
+				'fstreet' => $this->input->post('fstreet'),
+				'fprovince' => $this->input->post('fprovince'),
+				'fzipcode'=> $this->input->post('fzipcode'),
+				'ftel' => $this->input->post('ftel'),
+				'ffax' => $this->input->post('ffax'),
+				'fmail' => $this->input->post('fmail'),
+
+				'fname2' => $this->input->post('fname2'),
+				'fowner' => $this->input->post('fowner'),
+				'faddress2' => $this->input->post('faddress2'),
+				'fstreet2' => $this->input->post('fstreet2'),
+				'fprovince' => $this->input->post('fprovince'),
+				'fzipcode2' => $this->input->post('fzipcode2'),
+				'ftel2' => $this->input->post('ftel2'),
+				'fphone2' => $this->input->post('ffax2'),
+				'ffax2' => $this->input->post('ffax2'),
+
+				'flocation' => $this->input->post('flocation'),
+				'faddress3' => $this->input->post('faddress3'),
+				'fstreet3' => $this->input->post('fstreet3'),
+				'fzipcode3' => $this->input->post('fzipcode3'),
+				'ftel3' => $this->input->post('ftel3'),
+				'fphone3' => $this->input->post('fphone3'),
+				'ffax3' => $this->input->post('ffax3'),
+
+				'plantname' => $this->input->post('plantname'),
+				'sname' => $this->input->post('sname'),
+				'numberbreed' => $this->input->post('numberbreed'),
+				'numbercom' => $this->input->post('numbercom'),
+				'source' => $this->input->post('source'),
+				'ab' => $this->input->post('ab'),
+				);
+			//open preview form15
+			$this->load->view('pages/previews/preview15');
+		} else{
+			//redirect them to the login page
+		 	redirect('auth', 'refresh');
+		}
+		
+	}
 
 
 
